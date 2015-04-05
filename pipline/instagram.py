ccdcbb8aeb4b4987ba3460009806e684cda3e29e
siracoj <siraco.jared@gmail.com>
2015-04-05 14:17:29 -0400

    instagram pipeline skeleton and settings file

---
+++ pipline/instagram.py
@@ -0,0 +1,33 @@
+__author__ = 'jsiraco'
+
+import settings
+
+
+class InstagramPipline():
+
+    data = {}
+
+    def __init__(self):
+        """
+        Logs in, sets needed variables to get instagram data
+        :return:
+        """
+
+        #TODO: Add login and data grab logic
+
+    def run(self):
+        """
+        Runs the pipeline
+        grabs data about posts and adds it to the data JSON object
+        :return:
+        """
+
+        #TODO: Logic to get data, enforce request limits, and filter out duplicates
+
+    def save(self):
+        """
+        Downloads and saves instagram content(MongoDB?)
+        :return:
+        """
+
+        #TODO:Find place to save data, write logic to save images(Filter out video?)
---
+++ settings.py
@@ -0,0 +1,9 @@
+__author__ = 'jsiraco'
+
+
+CLIENT_ID = "32a5ddcbadd8408b9cca6937ee49df95"
+CLIENT_SECRET = "48872a7263db4232a5c48ee61b1945f0"
+
+WEBSITE_URL = "http://github.com/siracoj"
+
+REDIRECT_URI = "http://informativemurdercartoons.com"
