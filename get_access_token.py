7d837d62a0690ea4a9ffce085e5c65f273983598
siracoj <siraco.jared@gmail.com>
2015-04-25 15:46:28 -0400

    added get access token

---
+++ get_access_token.py
@@ -0,0 +1,30 @@
+from instagram.client import InstagramAPI
+import sys
+
+
+# Fix Python 2.x.
+try:
+    import __builtin__
+    input = getattr(__builtin__, 'raw_input')
+except (ImportError, AttributeError):
+    pass
+
+client_id = input("Client ID: ").strip()
+client_secret = input("Client Secret: ").strip()
+redirect_uri = input("Redirect URI: ").strip()
+raw_scope = input("Requested scope (separated by spaces, blank for just basic read): ").strip()
+scope = raw_scope.split(' ')
+# For basic, API seems to need to be set explicitly
+if not scope or scope == [""]:
+    scope = ["basic"]
+
+api = InstagramAPI(client_id=client_id, client_secret=client_secret, redirect_uri=redirect_uri)
+redirect_uri = api.get_authorize_login_url(scope = scope)
+
+print ("Visit this page and authorize access in your browser: "+ redirect_uri)
+
+code = (str(input("Paste in code in query string after redirect: ").strip()))
+
+access_token = api.exchange_code_for_access_token(code)
+print ("access token: " )
+print (access_token)
